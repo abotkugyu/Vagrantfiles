9a434931ee5a7948f03509d8c060d4afa253dd57
abotkugyu <abot_kugyu@yahoo.co.jp>
2017-09-15 18:53:29 +0900

    fix docker for apache spark

---
+++ repos/spark/containers/apache/conf/.bash_profile
@@ -0,0 +1,4 @@
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+export PATH=$JAVA_HOME:$PATH
+export SPARK_HOME=/usr/local/lib/spark
+export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
---
+++ repos/spark/data/script/sp.scala
@@ -0,0 +1,7 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+val textFile = sc.textFile("hdfs://...")
+val counts = textFile.flatMap(line => line.split(" "))
+                 .map(word => (word, 1))
+                 .reduceByKey(_ + _)
+counts.saveAsTextFile("hdfs://...")
